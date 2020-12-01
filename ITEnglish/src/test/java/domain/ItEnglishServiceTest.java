e33b8fe64446a9fb2ced6eccbb76a9dc356408dd
Tuomas <tietotuomas@gmail.com>
2020-12-01 06:48:23 +0200

    Create tests

--- ITEnglish/src/test/java/domain/ItEnglishServiceTest.java
+++ ITEnglish/src/test/java/domain/ItEnglishServiceTest.java
@@ -5,7 +5,13 @@
  */
 package domain;
 
+import itenglish.dao.FileVocabularyDao;
+import itenglish.dao.VocabularyDao;
 import itenglish.domain.ItEnglishService;
+import itenglish.domain.Vocabulary;
+import java.io.FileInputStream;
+import java.util.HashMap;
+import java.util.Properties;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -20,36 +26,77 @@ import static org.junit.Assert.*;
  */
 public class ItEnglishServiceTest {
 
+    private VocabularyDao vocabularyDao;
     private ItEnglishService service;
 
-    public ItEnglishServiceTest() {
-        this.service = new ItEnglishService();
+    public ItEnglishServiceTest() throws Exception {
+        Properties properties = new Properties();
+
+        properties.load(new FileInputStream("config.properties"));
+
+        HashMap<String, String> files = new HashMap<>();
+        files.put("beginner", properties.getProperty("beginner"));
+        files.put("average", properties.getProperty("average"));
+        files.put("master", properties.getProperty("master"));
+        FileVocabularyDao fileVocabularyDao = new FileVocabularyDao(files);
+        this.vocabularyDao = fileVocabularyDao;
+        this.service = new ItEnglishService(vocabularyDao);
     }
 
     @Before
     public void setUp() {
-        service.readFiles();
 
     }
 
     @Test
-    public void checkUserInputReturnsTrueIfDifficultyBeginner() {
-        assertTrue(this.service.checkUserInput("computer", "tietokone", "Aloittelija"));
+    public void checkUserInputReturnsCorrectStringIfDifficultyBeginner() {
+        assertEquals("Vastauksesi \"" + "file" + "\" oli oikein!", this.service.checkUserInput("file", "tiedosto", "Aloittelija"));
     }
 
     @Test
-    public void checkUserInputReturnsTrueIfDifficultyAverage() {
-        assertTrue(this.service.checkUserInput("simulation", "simulaatio", "Keskiverto"));
+    public void checkUserInputReturnsCorrectStringIfDifficultyAverage() {
+        assertEquals("Vastauksesi \"" + "input" + "\" oli oikein!", this.service.checkUserInput("input", "syöte", "Keskiverto"));
     }
 
     @Test
-    public void checkUserInputReturnsTrueIfDifficultyMaster() {
-        assertTrue(this.service.checkUserInput("checksum", "tarkistussumma", "Mestari"));
+    public void checkUserInputReturnsCorrectStringIfDifficultyMaster() {
+        assertEquals("Vastauksesi \"" + "heap" + "\" oli oikein!", this.service.checkUserInput("heap", "keko", "Mestari"));
     }
 
     @Test
-    public void checkUserInputReturnsFalseIfWrongInput() {
-        assertFalse(this.service.checkUserInput("checksumm", "tarkistussumma", "Mestari"));
+    public void checkUserInputReturnsCorrectStringIfDifficultyBeginnerAndWrongAnswer() {
+        assertEquals("Vastauksesi \"" + "wrong" + "\" oli väärin!\nOikea vastaus oli file.", this.service.checkUserInput("wrong", "tiedosto", "Aloittelija"));
     }
 
+    @Test
+    public void checkUserInputReturnsCorrectStringIfDifficultyAverageAndWrongAnswer() {
+        assertEquals("Vastauksesi \"" + "wrong" + "\" oli väärin!\nOikea vastaus oli input.", this.service.checkUserInput("wrong", "syöte", "Keskiverto"));
+    }
+
+    @Test
+    public void checkUserInputReturnsCorrectStringIfDifficultyMasterAndWrongAnswer() {
+        assertEquals("Vastauksesi \"" + "wrong" + "\" oli väärin!\nOikea vastaus oli heap.", this.service.checkUserInput("wrong", "keko", "Mestari"));
+    }
+
+    @Test
+    public void checkIfAcronymReturnsTrueIfWordStartsWithCapitalLetter() {
+        assertTrue(this.service.checkIfAcronym("FBI"));
+    }
+
+    @Test
+    public void checkIfAcronymReturnsFalseIfWordSDoesntStartWithCapitalLetter() {
+        assertFalse(this.service.checkIfAcronym("kameli"));
+    }
+
+    @Test
+    public void infoTextReturnsCorrectStringIfWordIsAcronym() {
+        assertEquals("Kirjoita auki lyhenne:", this.service.infoText("FBI"));
+
+    }
+
+    @Test
+    public void infoTextReturnsCorrectStringIfWordIsNotAcronym() {
+        assertEquals("Käännä englanniksi:", this.service.infoText("kameli"));
+
+    }
 }
